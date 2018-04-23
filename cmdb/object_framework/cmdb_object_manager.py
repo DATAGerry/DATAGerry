0cc5f2939a764b47948658beb5a2f5755eba27b1
markheumueller <mark.heumueller@gmx.de>
2018-04-23 07:58:39 +0200

    save local

--- cmdb/object_framework/cmdb_object_manager.py
+++ cmdb/object_framework/cmdb_object_manager.py
@@ -1,4 +1,19 @@
+from cmdb.data_storage import DATABASE_MANAGER
+
+
 class CmdbObjectManager:
 
-    def __init__(self, database_manager):
-        self.dbm = database_manager
+    def __init__(self, database_manager=None):
+        if not database_manager:
+            self.dbm = database_manager
+        else:
+            self.dbm = DATABASE_MANAGER
+
+    def get_object(self, public_id: int):
+        pass
+
+    def get_type(self, public_id: int):
+        pass
+
+    def get_category(self, public_id: int):
+        pass
