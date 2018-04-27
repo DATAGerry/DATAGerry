1c7b2c43392312dcce66e625172832bd0e6f3787
Mark <mark.heumueller@gmx.de>
2018-04-27 15:53:48 +0200

    adding security manager

--- cmdb/application_utils/__init__.py
+++ cmdb/application_utils/__init__.py
@@ -1,6 +1,9 @@
 from cmdb.application_utils.system_reader import SystemConfigReader, SystemSettingsReader
+from cmdb.application_utils.system_writer import SystemSettingsWriter
+from cmdb.application_utils.security import SecurityManager
 from cmdb.data_storage.database_connection import MongoConnector
 from cmdb.data_storage.database_manager import DatabaseManagerMongo
+
 SYSTEM_CONFIG_READER = SystemConfigReader(
     config_name=SystemConfigReader.DEFAULT_CONFIG_NAME,
     config_location=SystemConfigReader.DEFAULT_CONFIG_LOCATION
@@ -17,3 +20,20 @@ SYSTEM_SETTINGS_READER = SystemSettingsReader(
     )
 
 )
+
+SYSTEM_SETTINGS_WRITER = SystemSettingsWriter(
+    database_manager=DatabaseManagerMongo(
+        connector=MongoConnector(
+            host=SYSTEM_CONFIG_READER.get_value('host', 'Database'),
+            port=int(SYSTEM_CONFIG_READER.get_value('port', 'Database')),
+            database_name=SYSTEM_CONFIG_READER.get_value('database_name', 'Database'),
+            timeout=MongoConnector.DEFAULT_CONNECTION_TIMEOUT
+        )
+    )
+)
+
+SECURITY_MANAGER = SecurityManager(
+    SYSTEM_SETTINGS_READER,
+    SYSTEM_SETTINGS_WRITER
+)
+
