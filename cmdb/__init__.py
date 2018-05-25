4c425fe60f9d5b01fd80567d291df5db872db936
Mark <mark.heumueller@gmx.de>
2018-05-25 12:19:14 +0200

    save dev status

---
+++ cmdb/__init__.py
@@ -0,0 +1,6 @@
+__title__ = 'cmdb'
+__version__ = '1.0.0'
+__author__ = 'NETHINKS GmbH'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2018 NETHINKS GmbH'
+__DEBUG__ = False
--- cmdb/__main__.py
+++ cmdb/__main__.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """
 Net|CMDB is a flexible asset management tool and
 open-source configurable management database
@@ -5,7 +6,32 @@ open-source configurable management database
 You should have received a copy of the MIT License along with Net|CMDB.
 If not, see <https://github.com/NETHINKS/NetCMDB/blob/master/LICENSE>.
 """
+from cmdb import __version__, __title__, __DEBUG__
+from optparse import OptionParser
+
+
+def build_argparser():
+    _parser = OptionParser(
+        usage="usage: {} [options]".format(__title__),
+        version=" {}".format(__version__)
+    )
+    _parser.add_option("-d", "--debug", action="callback", callback=dev,
+                       help="enable debug mode: DO NOT USE ON PRODUCTIVE SYSTEMS")
+    (options, args) = _parser.parse_args()
+    return _parser
+
+
+def dev(*args):
+    """
+    Quick test function
+    """
+    global __DEBUG__
+    __DEBUG__ = True
+
+
+def main(args):
+    pass
 
 
 if __name__ == "__main__":
-    pass
+    parser = build_argparser()
