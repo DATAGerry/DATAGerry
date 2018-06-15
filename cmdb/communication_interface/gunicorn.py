a3ae660f3e4a78111a033333a8ccd16ae7e81bb1
Mark <mark.heumueller@gmx.de>
2018-06-15 13:45:04 +0200

    [ADD] make multiapps starts in gunicorn

--- cmdb/communication_interface/gunicorn.py
+++ cmdb/communication_interface/gunicorn.py
@@ -37,3 +37,28 @@ class HTTPServer(BaseApplication):
 
         import multiprocessing
         return (multiprocessing.cpu_count() * 2) + 1
+
+
+class DispatcherMiddleware:
+
+    def __init__(self, app, mounts=None):
+        self.app = app
+        self.mounts = mounts or {}
+
+    def __call__(self, environ, start_response):
+        script = environ.get('PATH_INFO', '')
+        path_info = ''
+        while '/' in script:
+            if script in self.mounts:
+                app = self.mounts[script]
+                break
+            script, last_item = script.rsplit('/', 1)
+            path_info = '/%s%s' % (last_item, path_info)
+        else:
+            app = self.mounts.get(script, self.app)
+        original_script_name = environ.get('SCRIPT_NAME', '')
+        environ['SCRIPT_NAME'] = original_script_name + script
+        environ['PATH_INFO'] = path_info
+        return app(environ, start_response)
+
+
