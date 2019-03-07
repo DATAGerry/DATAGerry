a579a0dde3f09e85ceff705d4012a592239d0e59
Michael Batz <michael.batz@nethinks.com>
2019-03-07 17:25:25 +0100

    exportd: disable testing

--- cmdb/exportd/service.py
+++ cmdb/exportd/service.py
@@ -14,8 +14,9 @@ class ExportdService(cmdb.process_management.service.AbstractCmdbService):
 
     def _run(self):
         # ToDo: for testing only
-        time.sleep(5)
-        self.__schedule_job()
+        #time.sleep(5)
+        #self.__schedule_job()
+        pass
 
     def _handle_event(self, event):
         event_type = event.get_type()
