15e5b961bd692c00cd5f4f63f990f6327f630063
Sergej Dumler <sergej.dumler@nethinks.com>
2019-05-10 10:20:24 +0200

    [NET-21] [NET-47] Corrected output for author in object list.

--- cmdb/object_framework/cmdb_render.py
+++ cmdb/object_framework/cmdb_render.py
@@ -253,9 +253,8 @@ class CmdbRender:
             if element.has_externals():
                 render_result.set_externals(element.get_externals(True))
 
-            if users_dict[0]['first_name'] and users_dict[0]['last_name']:
-                render_result.set_author_name(users_dict[0]['first_name']+" "+users_dict[0]['last_name'])
-
+            dic = [i for i in users_dict if (i['public_id'] == element.object_instance.author_id)]
+            render_result.set_author_name('{} {}'.format(dic[0]['first_name'], dic[0]['last_name']))
             render_list.append(render_result)
 
         return render_list
