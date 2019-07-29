d83ae164d1fac76c9e2a1a536d0975ca8f038723
Sergej Dumler <sergej.dumler@nethinks.com>
2019-07-29 11:06:23 +0200

    [NET-22][NET-86] JSON format output adapted to XML output

--- cmdb/file_export/json_file_exporter.py
+++ cmdb/file_export/json_file_exporter.py
@@ -14,11 +14,27 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import operator
 from cmdb.file_export.file_exporter import FileExporter
 from cmdb.interface.route_utils import make_response
+from cmdb.object_framework.cmdb_object_manager import object_manager
 
 
 class JsonFileExporter(FileExporter):
 
     def main(self):
-        self.set_response(make_response(self.get_object_list()).data)
+        object_list = self.get_object_list()
+        new_dict = {'public_id': 0, 'active': True, 'type': '', 'fields': []}
+        s_keys = ['public_id', 'active', 'type_id', 'fields']
+        filter_dict = []
+
+        for obj in object_list:
+            for k in s_keys:
+                if k in obj.__dict__:
+                    if k == 'type_id':
+                        new_dict.update({'type': object_manager.get_type(obj.type_id).label})
+                    else:
+                        new_dict.update({k: obj.__dict__[k]})
+
+            filter_dict.append(new_dict)
+        self.set_response(make_response(filter_dict).data)
