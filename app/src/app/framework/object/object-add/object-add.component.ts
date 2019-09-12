6a117d0c9818050760d4a16f645819a4b8a8bd08
Sergej Dumler <sergej.dumler@nethinks.com>
2019-09-12 14:54:51 +0200

    [Bugfix] Add new object grouping by Categorylist.

--- app/src/app/framework/object/object-add/object-add.component.ts
+++ app/src/app/framework/object/object-add/object-add.component.ts
@@ -86,10 +86,8 @@ export class ObjectAddComponent implements OnInit, OnDestroy {
       this.categoryService.getCategoryList().subscribe((categoryList: CmdbCategory[]) => {
         categoryList.forEach( category => {
           for ( const type of this.typeList ) {
-            if (type.category_id === category.public_id) {
-              type.category_name = category.label;
-              this.preperatedTypeList.push(type);
-            } else if (category.root && type.category_id === 0) {
+            if ((type.category_id === category.public_id)
+              || (category.root && type.category_id === 0) ) {
               type.category_name = category.label;
               this.preperatedTypeList.push(type);
             }
