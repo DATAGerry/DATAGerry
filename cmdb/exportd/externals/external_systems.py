390dd663849a79c9d9101568ee4c4ad8561d9ff7
Michael Batz <michael.batz@nethinks.com>
2019-11-19 21:14:20 +0100

    [NET-18] start with implementing ExternalSystemOpenNMS

--- cmdb/exportd/externals/external_systems.py
+++ cmdb/exportd/externals/external_systems.py
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import xml.etree.ElementTree as ET
 from cmdb.exportd.exporter_base import ExternalSystem, ExportJobConfigException, ExportVariable
 
 
@@ -57,3 +58,50 @@ class ExternalSystemDummy(ExternalSystem):
 
     def finish_export(self):
         print("finish export")
+
+
+
+class ExternalSystemOpenNMS(ExternalSystem):
+
+    parameters = [
+        {"name": "resturl", "required": True, "description": "OpenNMS REST URL", "default": "http://127.0.0.1:8980/opennms/rest"},
+        {"name": "restuser", "required": True, "description": "OpenNMS REST user", "default": "admin"},
+        {"name": "restpassword", "required": True, "description": "OpenNMS REST password", "default": "admin"},
+        {"name": "requisition", "required": True, "description": "OpenNMS requisition to use", "default": "cmdb"},
+        {"name": "services", "required": False, "description": "name of services to bind on each node sepetated by space", "default": "ICMP SNMP"},
+        {"name": "sslVerify", "required": False, "description": "disable SSL peer verification", "default": "false"},
+        {"name": "rescanExisting", "required": False, "description": "set rescanExisting flag for OpenNMS import", "default": "dbOnly"},
+        {"name": "exportSnmpConfig", "required": False, "description": "also export SNMP configuration for nodes", "default": "false"},
+        {"name": "exportSnmpConfigRetries", "required": False, "description": "export SNMP configuration for nodes: set SNMP retries", "default": "1"},
+        {"name": "exportSnmpConfigTimeout", "required": False, "description": "export SNMP configuration for nodes: set SNMP timeout", "default": "2000"}
+    ]
+
+    def __init__(self, destination_parms, export_vars):
+        super(ExternalSystemOpenNMS, self).__init__(destination_parms, export_vars)
+        # ToDo: init destination vars
+        self.__requisition = self._destination_parms.get("requisition", None)
+        if not (self.__requisition):
+            raise ExportJobConfigException()
+
+
+    def prepare_export(self):
+        # init XML object for OpenNMS REST API
+        attributes = {}
+        attributes["foreign-source"] = self.__requisition
+        self.__xml = ET.Element("model-import", attributes)
+
+    def add_object(self, cmdb_object):
+        # get node variables
+        node_foreignid = cmdb_object.get_public_id()
+        node_label = self._export_vars.get("nodelabel", ExportVariable("nodelabel", "undefined")).get_value(cmdb_object)
+
+        # append node info to XML structure
+        node_xml_attr = {}
+        node_xml_attr["foreign-id"] = str(node_foreignid)
+        node_xml_attr["node-label"] = str(node_label)
+        node_xml = ET.Element("node", node_xml_attr)
+        self.__xml.append(node_xml)
+
+
+    def finish_export(self):
+        print(ET.tostring(self.__xml, encoding="unicode", method="xml"))
