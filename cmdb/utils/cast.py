59b022e7388edc39f050f5e5efc5f234f2ded8b5
Mark Heumüller <mark.heumueller@nethinks.com>
2019-11-29 15:47:01 +0100

    [NET-9] Add autocast function for import value validation

---
+++ cmdb/utils/cast.py
@@ -0,0 +1,39 @@
+# DATAGERRY - OpenSource Enterprise CMDB
+# Copyright (C) 2019 NETHINKS GmbH
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+import ast
+
+
+def boolify(s):
+    if s == 'True' or s == 'true':
+        return True
+    if s == 'False' or s == 'false':
+        return False
+    raise ValueError('Not Boolean Value!')
+
+
+def noneify(s):
+    if s == 'None' or s == 'null':
+        return None
+    raise ValueError('Not None Value!')
+
+
+def auto_cast(val):
+    for caster in (boolify, int, noneify, float, str):
+        try:
+            return caster(val)
+        except ValueError:
+            pass
+    return val
