41b5863114fd8de277dcafc49dcdc8d464fc8d7a
Sergej Dumler <sergej.dumler@nethinks.com>
2019-12-04 15:38:17 +0100

    [NET-13][NET-225] support resolving of object-ref in export variables.

--- cmdb/exportd/exporter_base.py
+++ cmdb/exportd/exporter_base.py
@@ -19,7 +19,7 @@ import logging
 
 from cmdb.data_storage.database_manager import DatabaseManagerMongo
 from cmdb.exportd.exportd_job.exportd_job_manager import ExportdJobManagement
-from cmdb.exportd.exportd_logs.exportd_log_manager import ExportdLogManager
+from cmdb.exportd.exportd_logs.exportd_log_manager import ExportdLogManager, LogManagerDeleteError
 from cmdb.exportd.exportd_job.exportd_job import ExportdJob
 from cmdb.utils.error import CMDBError
 from cmdb.utils.helpers import load_class
@@ -112,6 +112,16 @@ class ExportVariable:
         self.__name = name
         self.__value_tpl_default = value_tpl_default
         self.__value_tpl_types = value_tpl_types
+        self.__iteration = 3
+        self.__objectdata = {}
+
+        scr = SystemConfigReader()
+        database_manager = DatabaseManagerMongo(
+            **scr.get_all_values_from_section('Database')
+        )
+        self.__object_manager = CmdbObjectManager(
+            database_manager=database_manager
+        )
 
     def get_value(self, cmdb_object):
         # get value template
@@ -123,21 +133,47 @@ class ExportVariable:
                 value_template = templ['template']
 
         # objectdata for use in ExportVariable templates
-        objectdata = {'id': cmdb_object.object_information['object_id']}
+        self.__objectdata.update({'fields': {}})
+        self.__objectdata.update({'id': cmdb_object.object_information['object_id']})
         # objectdata: object fields
         # ToDo: use rendered fields
-        objectdata["fields"] = {}
         fields = cmdb_object.fields
         for field in fields:
-            field_name = field["name"]
-            field_value = field["value"]
-            objectdata["fields"][field_name] = field_value
+            # field_name = field["name"]
+            # field_value = field["value"]
+            # objectdata["fields"][field_name] = field_value
+            self.set_references(field)
         # ToDo: dereference objectrefs
 
         # render template
         template = jinja2.Template(value_template)
-        output = template.render(objectdata)
+        try:
+            output = template.render(self.__objectdata)
+        except Exception as ex:
+            LOGGER.warning(ex)
+            output = ''
         return output
+
+    def set_references(self, field):
+
+        if 'ref' == field["type"] and field["value"] and self.__iteration != 0:
+            self.__iteration = self.__iteration-1
+            current_object = self.__object_manager.get_object(field["value"])
+            type_instance = self.__object_manager.get_type(current_object.get_type_id())
+            cmdb_render_object = CmdbRender(object_instance=current_object, type_instance=type_instance, render_user=None)
+            sub_fields = {field["name"]: {'id': field["value"], 'fields': {}}}
+            for subfield in cmdb_render_object.result().fields:
+                field_name = subfield["name"]
+                field_value = subfield["value"]
+                update = {field_name: field_value}
+                sub_fields[field["name"]]['fields'].update(update)
+                self.__objectdata['fields'].update(sub_fields)
+                self.set_references(subfield)
+        else:
+            field_name = field["name"]
+            field_value = field["value"]
+            update = {field_name: field_value}
+            self.__objectdata['fields'].update(update)
 
 
 class ExportSource:
