2420ffd1dc2a830fad9808cc69aadae13cc43ffc
Michael Batz <michael.batz@nethinks.com>
2019-12-30 09:48:07 +0100

    [NET-257] fix adding objects with a specific public_id

--- cmdb/interface/rest_api/framework_routes/object_routes.py
+++ cmdb/interface/rest_api/framework_routes/object_routes.py
@@ -406,8 +406,10 @@ def insert_object(request_user: User):
 
     try:
         new_object_data = json.loads(add_data_dump, object_hook=json_util.object_hook)
-        new_object_data['public_id'] = object_manager.get_new_id(CmdbObject.COLLECTION)
-        new_object_data['active'] = True
+        if not 'public_id' in new_object_data:
+            new_object_data['public_id'] = object_manager.get_new_id(CmdbObject.COLLECTION)
+        if not 'active' in new_object_data:
+            new_object_data['active'] = True
         new_object_data['creation_time'] = datetime.utcnow()
         new_object_data['views'] = 0
         new_object_data['version'] = '1.0.0'  # default init version
