35ef5b28910aa84de97c496d670eb57eb96699e6
Michael Batz <michael.batz@nethinks.com>
2020-06-15 20:07:08 +0200

    [NET-409] DocAPI: handle image upload in TinyMCE

--- app/src/app/settings/docapi-settings/docapi-settings-builder/docapi-settings-builder-content-step/docapi-settings-builder-content-step.component.ts
+++ app/src/app/settings/docapi-settings/docapi-settings-builder/docapi-settings-builder-content-step/docapi-settings-builder-content-step.component.ts
@@ -20,6 +20,9 @@ import { Component, OnInit, Input } from '@angular/core';
 import { CmdbMode } from '../../../../framework/modes.enum';
 import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 
+
+declare var tinymce;
+
 @Component({
   selector: 'cmdb-docapi-settings-builder-content-step',
   templateUrl: './docapi-settings-builder-content-step.component.html',
@@ -30,6 +33,44 @@ export class DocapiSettingsBuilderContentStepComponent implements OnInit {
   @Input() public mode: CmdbMode;
   public modes = CmdbMode;
   public contentForm: FormGroup;
+
+  public editorConfig = {
+    base_url: '/assets/tinymce',
+    suffix: '.min',
+    height: 500,
+    menubar: false,
+    plugins: [
+      'advlist autolink lists link image charmap print preview anchor',
+      'searchreplace visualblocks code fullscreen',
+      'insertdatetime media table paste code help wordcount'
+    ],
+    toolbar:
+      'undo redo | formatselect | bold italic backcolor | \
+      alignleft aligncenter alignright alignjustify | \
+      bullist numlist outdent indent | image | removeformat | help',
+    paste_data_images: true,
+    automatic_uploads: true,
+    file_picker_types: 'image',
+    file_picker_callback: function (cb, value, meta) {
+      let input = document.createElement('input');
+      input.setAttribute('type', 'file');
+      input.setAttribute('accept', 'image/*');
+      input.onchange = function () {
+        let file = input.files[0];
+        let reader = new FileReader();
+        reader.onload = function () {
+          let id = 'blobid' + (new Date()).getTime();
+          let blobCache = tinymce.activeEditor.editorUpload.blobCache;
+          let base64 = (<string>reader.result).split(',')[1];
+          let blobInfo = blobCache.create(id, file, base64);
+          blobCache.add(blobInfo);
+          cb(blobInfo.blobUri(), { title: file.name });
+        };
+        reader.readAsDataURL(file);
+      };
+      input.click();
+    }
+  }
 
   constructor() { 
     this.contentForm = new FormGroup({
