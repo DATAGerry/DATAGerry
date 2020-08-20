7f1f3d10f8e374e7373f95371624fc7e747ed1e3
Mark Heumüller <mark.heumueller@nethinks.com>
2020-08-20 09:53:37 +0200

    [NET-441] Update api_parameters.py docs

--- cmdb/interface/api_parameters.py
+++ cmdb/interface/api_parameters.py
@@ -32,10 +32,22 @@ class ApiParameters:
 
 
 class CollectionParameters(ApiParameters):
-    """Rest API class for collection parsing"""
+    """Rest API class for parameters passed by a http request on a collection route"""
 
     def __init__(self, query_string: Parameter, limit: int = None, sort: str = None,
                  order: int = None, page: int = None, filter: Union[List[dict], dict] = None, **kwargs):
+        """
+        Constructor of the CollectionParameters.
+
+        Args:
+            query_string: The raw http query string. Can be used when the parsed parameters are not enough.
+            limit: The max number of resources returned (pageSize).
+            sort: The query element which is used as the sort id (nested resources are possible via (.) dot).
+            order: The order sequence in which `way` the sort should be returned.
+            page: The current page. N number of elements will be skip based on (limit * page)
+            filter: A generic query filter based on https://docs.mongodb.com/compass/master/query/filter/
+            **kwargs:
+        """
         self.limit: int = int(limit or 10)
         self.sort: str = sort or Parameter('public_id')
         self.order: int = int(order or SortOrder.ASCENDING.value)
@@ -48,6 +60,7 @@ class CollectionParameters(ApiParameters):
     def from_http(cls, query_string: str, **parameters) -> "CollectionParameters":
         """
         Create a collection parameter instance from a http query string
+
         Args:
             query_string: raw query string
             **parameters: list of optional http parameters
