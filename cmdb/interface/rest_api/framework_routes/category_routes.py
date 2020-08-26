c050fe5813b13cbf06ac06affb260b03d730e77b
Mark Heumüller <mark.heumueller@nethinks.com>
2020-08-26 11:50:47 +0200

    [NET-440] DOCS fix category rest documentation

--- cmdb/interface/rest_api/framework_routes/category_routes.py
+++ cmdb/interface/rest_api/framework_routes/category_routes.py
@@ -67,7 +67,7 @@ def get_categories(params: CollectionParameters):
        .. sourcecode:: http
 
           GET /rest/categories/ HTTP/1.1
-          Host: datagerry.co
+          Host: datagerry.com
           Accept: application/json
 
        **Example response**:
@@ -78,6 +78,7 @@ def get_categories(params: CollectionParameters):
           Content-Type: application/json
           Content-Length: 3311
           X-Total-Count: 1
+          X-API-Version: 1.0
 
           {
               "results": [
@@ -162,6 +163,63 @@ def get_categories(params: CollectionParameters):
 @categories_blueprint.route('/<int:public_id>', methods=['GET', 'HEAD'])
 @categories_blueprint.protect(auth=True, right='base.framework.category.view')
 def get_category(public_id: int):
+    """
+    HTTP `GET`/`HEAD` route for a single category resource.
+
+    Args:
+        public_id (int): Public ID of the category.
+
+    Raises:
+        ManagerGetError: When the selected category does not exists.
+
+    Returns:
+        GetSingleResponse: Which includes the json data of a CategoryDAO.
+
+    .. http:get:: /category/(int:public_id)
+
+        The category with the public_id.
+
+        **Example request**
+
+        .. sourcecode:: http
+
+            GET /rest/categories/1 HTTP/1.1
+            Host: datagerry.com
+            Accept: application/json
+
+        **Example response**
+
+        .. sourcecode:: http
+
+            HTTP/1.1 200 OK
+            Content-Type: application/json
+            Content-Length: 588
+            X-API-Version: 1.0
+
+            {
+              "result": {
+                "public_id": 1,
+                "name": "example",
+                "label": "Example",
+                "meta": {
+                  "icon": "far fa-folder-open",
+                  "order": 0
+                },
+                "parent": null,
+                "types": [1]
+              },
+              "response_type": "GET",
+              "model": "Category",
+              "time": "2020-08-20T09:21:10.235525"
+            }
+
+        :reqheader Accept: application/json
+        :reqheader Authorization: jwtoken to authenticate
+        :resheader Content-Type: application/json
+        :statuscode 200: Everything is fine.
+        :statuscode 404: No resource found.
+
+    """
     category_manager: CategoryManager = CategoryManager(database_manager=current_app.database_manager)
     body = True if not request.method != 'HEAD' else False
 
@@ -178,6 +236,77 @@ def get_category(public_id: int):
 @categories_blueprint.protect(auth=True, right='base.framework.category.add')
 @categories_blueprint.validate(CategoryDAO.SCHEMA)
 def insert_category(data: dict):
+    """
+    HTTP `POST` route for insert a single category resource.
+
+    Args:
+        data (CategoryDAO.SCHEMA): Insert data of a new category.
+
+    Raises:
+        ManagerGetError: If the inserted resource could not be found after inserting.
+        ManagerInsertError: If something went wrong during insertion.
+
+    Returns:
+        InsertSingleResponse: Insert response with the new category and its public_id.
+
+    .. http:post:: /category/
+
+        HTTP Post route for inserting a new category.
+
+        **Example request**
+
+        .. sourcecode:: http
+
+            POST /rest/categories/ HTTP/1.1
+            Host: datagerry.com
+            Accept: application/json
+
+            {
+              "name": "example",
+              "label": "Example",
+              "meta": {
+                "icon": "",
+                "order": 0
+              },
+              "parent": null,
+              "types": [1]
+            }
+
+        **Example response**
+
+        .. sourcecode:: http
+
+            HTTP/1.1 200 OK
+            Content-Type: application/json
+            Content-Length: 588
+            Location: http://localhost:4000/rest/categories/1
+            X-API-Version: 1.0
+
+            {
+              "result_id": 1,
+              "raw": {
+                "public_id": 1,
+                "name": "example",
+                "label": "Example",
+                "meta": {
+                  "icon": "",
+                  "order": 0
+                },
+                "parent": null,
+                "types": [1]
+              },
+              "response_type": "INSERT",
+              "model": "Category",
+              "time": "2020-08-20T11:14:42.704920"
+            }
+
+        :reqheader Accept: application/json
+        :reqheader Authorization: jwtoken to authenticate
+        :resheader Content-Type: application/json
+        :statuscode 200: Everything is fine.
+        :statuscode 400: Resource could not be inserted.
+        :statuscode 404: No resource found.
+    """
     category_manager: CategoryManager = CategoryManager(database_manager=current_app.database_manager)
     try:
         result_id: PublicID = category_manager.insert(data)
@@ -195,6 +324,78 @@ def insert_category(data: dict):
 @categories_blueprint.protect(auth=True, right='base.framework.category.edit')
 @categories_blueprint.validate(CategoryDAO.SCHEMA)
 def update_category(public_id: int, data: dict):
+    """
+    HTTP `PUT`/`PATCH` route for update a single category resource.
+
+    Args:
+        public_id (int): Public ID of the updatable category
+        data (CategoryDAO.SCHEMA): New category data to update
+
+    Raises:
+        ManagerGetError: When the category with the `public_id` was not found.
+        ManagerUpdateError: When something went wrong during the update.
+
+    Returns:
+        UpdateSingleResponse: With update result of the new updated category.
+
+    .. http:put:: /category/(int:public_id)
+
+        HTTP `PUT`/`PATCH` route for updating a existing category.
+
+        **Example request**
+
+        .. sourcecode:: http
+
+            PUT /rest/categories/1 HTTP/1.1
+            Host: datagerry.com
+            Accept: application/json
+
+            {
+              ""public_id": 1,
+              "name": "example",
+              "label": "Example",
+              "meta": {
+                "icon": "",
+                "order": 0
+              },
+              "parent": null,
+              "types": [1]
+            }
+
+        **Example response**
+
+        .. sourcecode:: http
+
+            HTTP/1.1 202 ACCEOTED
+            Content-Type: application/json
+            Content-Length: 170
+            Location: http://localhost:4000/rest/categories/1
+            X-API-Version: 1.0
+
+            {
+              "result": {
+                "public_id": 1,
+                "name": "example2",
+                "label": "Example,
+                "meta": {
+                  "icon": "",
+                  "order": 0
+                },
+                "parent": null,
+                "types": []
+              },
+              "response_type": "UPDATE",
+              "model": "Category",
+              "time": "2020-08-20T11:37:07.499137"
+            }
+
+        :reqheader Accept: application/json
+        :reqheader Authorization: jwtoken to authenticate
+        :resheader Content-Type: application/json
+        :statuscode 202: Everything is fine.
+        :statuscode 400: Resource could not be updated.
+        :statuscode 404: No resource found.
+    """
     category_manager: CategoryManager = CategoryManager(database_manager=current_app.database_manager)
     try:
         category = CategoryDAO.from_data(data=data)
@@ -203,9 +404,9 @@ def update_category(public_id: int, data: dict):
     except ManagerGetError as err:
         return abort(404, err.message)
     except ManagerUpdateError as err:
-        return abort(400, err)
+        return abort(400, err.message)
     except Exception as err:
-        return abort(400, err)
+        return abort(400, err.message)
 
     return api_response.make_response()
 
@@ -213,6 +414,66 @@ def update_category(public_id: int, data: dict):
 @categories_blueprint.route('/<int:public_id>', methods=['DELETE'])
 @categories_blueprint.protect(auth=True, right='base.framework.category.delete')
 def delete_category(public_id: int):
+    """
+    HTTP `DELETE` route for delete a single category resource.
+
+    Args:
+        public_id (int): Public ID of the deletable category
+
+    Raises:
+        ManagerGetError: When the category with the `public_id` was not found.
+        ManagerDeleteError: When something went wrong during the deletion.
+
+    Returns:
+        DeleteSingleResponse: Delete result with the deleted category as data.
+
+    .. http:delete:: /category/(int:public_id)
+
+        HTTP `DELETE` route for deleting a existing category.
+
+        **Example request**
+
+        .. sourcecode:: http
+
+            DELETE /rest/categories/1 HTTP/1.1
+            Host: datagerry.com
+            Accept: application/json
+
+        **Example response**
+
+        .. sourcecode:: http
+
+            HTTP/1.1 202 ACCEOTED
+            Content-Type: application/json
+            Content-Length: 170
+            X-API-Version: 1.0
+
+            {
+              "deleted_entry": {
+                "public_id": 1,
+                "name": "example",
+                "label": "Eexample",
+                "meta": {
+                  "icon": "",
+                  "order": 1
+                },
+                "parent": null,
+                "types": [
+                  1
+                ]
+              },
+              "response_type": "DELETE",
+              "model": "Category",
+              "time": "2020-08-20T11:45:50.809706"
+            }
+
+        :reqheader Accept: application/json
+        :reqheader Authorization: jwtoken to authenticate
+        :resheader Content-Type: application/json
+        :statuscode 202: Everything is fine.
+        :statuscode 400: Resource could not be deleted.
+        :statuscode 404: No resource found.
+    """
     category_manager: CategoryManager = CategoryManager(database_manager=current_app.database_manager)
     try:
         deleted_category = category_manager.delete(public_id=PublicID(public_id))
