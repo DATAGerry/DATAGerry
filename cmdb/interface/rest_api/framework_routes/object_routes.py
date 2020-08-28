d8bf3029bf44557cc5d39bf5d93b24e40d4419bb
Mark Heumüller <mark.heumueller@nethinks.com>
2020-08-28 11:10:44 +0200

    [NET-337][NET-379] Quick fix for multi call error on types

--- cmdb/interface/rest_api/framework_routes/object_routes.py
+++ cmdb/interface/rest_api/framework_routes/object_routes.py
@@ -278,9 +278,6 @@ def get_objects_by_public_id(public_ids, request_user: User):
 
     try:
         filter_state = {'public_id': public_ids}
-        if _fetch_only_active_objs():
-            filter_state['active'] = {"$eq": True}
-
         query = _build_query(filter_state, q_operator='$or')
         all_objects_list = object_manager.get_objects_by(sort="public_id", **query)
         rendered_list = RenderList(all_objects_list, request_user).render_result_list()
