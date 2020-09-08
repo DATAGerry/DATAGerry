b3f3d15ddf86a64e043bed099cfc943a48560204
Dorus Blanken <dorusblanken@gmail.com>
2020-09-08 08:56:05 +0200

    [NET-447] Added Copyright and removed redundant classes

--- app/src/app/layout/components/info-box/info-box.component.ts
+++ app/src/app/layout/components/info-box/info-box.component.ts
@@ -1,18 +1,31 @@
-import {Component, Input, OnInit} from '@angular/core';
+/*
+* DATAGERRY - OpenSource Enterprise CMDB
+* Copyright (C) 2019 NETHINKS GmbH
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'cmdb-info-box',
   templateUrl: './info-box.component.html',
   styleUrls: ['./info-box.component.scss']
 })
-export class InfoBoxComponent implements OnInit {
+export class InfoBoxComponent {
 
   @Input() message: string;
   @Input() icon: string = 'far fa-file';
   @Input() doc: string = 'https://docs.datagerry.com';
-
-  constructor() { }
-
-  ngOnInit() {
-  }
 }
