75c0e9e4c4e05f85a835e67d16d968309e690539
Mark Heumüller <mark.heumueller@nethinks.com>
2020-09-17 10:17:52 +0200

    [NET-512][NET-519] Update type rest documentation and body validation

--- cmdb/interface/rest_api/framework_routes/type_routes.py
+++ cmdb/interface/rest_api/framework_routes/type_routes.py
@@ -15,6 +15,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import logging
+from datetime import datetime
 
 from flask import abort, request, current_app
 
@@ -37,8 +38,26 @@ types_blueprint = APIBlueprint('types', __name__)
 @types_blueprint.protect(auth=True, right='base.framework.type.view')
 @types_blueprint.parse_collection_parameters()
 def get_types(params: CollectionParameters):
+    """
+    HTTP `GET`/`HEAD` route for getting a iterable collection of resources.
+
+    Args:
+        params (CollectionParameters): Passed parameters over the http query string
+
+    Returns:
+        GetMultiResponse: Which includes a IterationResult of the TypeModel.
+
+    Example:
+        You can pass any parameter based on the CollectionParameters.
+        Optional parameters are passed over the function declaration.
+
+    Raises:
+        FrameworkIterationError: If the collection could not be iterated.
+        ManagerGetError: If the collection could not be found.
+
+    """
     type_manager = TypeManager(database_manager=current_app.database_manager)
-    body = True if not request.method != 'HEAD' else False
+    body = request.method == 'HEAD'
 
     try:
         iteration_result: IterationResult[TypeModel] = type_manager.iterate(
@@ -56,8 +75,20 @@ def get_types(params: CollectionParameters):
 @types_blueprint.route('/<int:public_id>', methods=['GET', 'HEAD'])
 @types_blueprint.protect(auth=True, right='base.framework.type.view')
 def get_type(public_id: int):
+    """
+    HTTP `GET`/`HEAD` route for a single type resource.
+
+    Args:
+        public_id (int): Public ID of the type.
+
+    Raises:
+        ManagerGetError: When the selected type does not exists.
+
+    Returns:
+        GetSingleResponse: Which includes the json data of a TypeModel.
+    """
     type_manager = TypeManager(database_manager=current_app.database_manager)
-    body = True if not request.method != 'HEAD' else False
+    body = request.method == 'HEAD'
 
     try:
         type_ = type_manager.get(public_id)
@@ -72,7 +103,21 @@ def get_type(public_id: int):
 @types_blueprint.protect(auth=True, right='base.framework.type.add')
 @types_blueprint.validate(TypeModel.SCHEMA)
 def insert_type(data: dict):
+    """
+    HTTP `POST` route for insert a single type resource.
+
+    Args:
+        data (TypeModel.SCHEMA): Insert data of a new type.
+
+    Raises:
+        ManagerGetError: If the inserted resource could not be found after inserting.
+        ManagerInsertError: If something went wrong during insertion.
+
+    Returns:
+        InsertSingleResponse: Insert response with the new type and its public_id.
+    """
     type_manager = TypeManager(database_manager=current_app.database_manager)
+    data.setdefault('creation_time', datetime.utcnow())
     try:
         result_id: PublicID = type_manager.insert(data)
         raw_doc = type_manager.get(public_id=result_id)
@@ -89,11 +134,25 @@ def insert_type(data: dict):
 @types_blueprint.protect(auth=True, right='base.framework.type.edit')
 @types_blueprint.validate(TypeModel.SCHEMA)
 def update_type(public_id: int, data: dict):
+    """
+    HTTP `PUT`/`PATCH` route for update a single type resource.
+
+    Args:
+        public_id (int): Public ID of the updatable type
+        data (TypeModel.SCHEMA): New type data to update
+
+    Raises:
+        ManagerGetError: When the type with the `public_id` was not found.
+        ManagerUpdateError: When something went wrong during the update.
+
+    Returns:
+        UpdateSingleResponse: With update result of the new updated type.
+    """
     type_manager = TypeManager(database_manager=current_app.database_manager)
     try:
-        type = TypeModel.from_data(data=data)
+        type_ = TypeModel.from_data(data=data)
 
-        type_manager.update(public_id=PublicID(public_id), type=TypeModel.to_json(type))
+        type_manager.update(public_id=PublicID(public_id), type=TypeModel.to_json(type_))
         api_response = UpdateSingleResponse(result=data, url=request.url, model=TypeModel.MODEL)
     except ManagerGetError as err:
         return abort(404, err.message)
@@ -106,6 +165,19 @@ def update_type(public_id: int, data: dict):
 @types_blueprint.route('/<int:public_id>', methods=['DELETE'])
 @types_blueprint.protect(auth=True, right='base.framework.type.delete')
 def delete_type(public_id: int):
+    """
+    HTTP `DELETE` route for delete a single type resource.
+
+    Args:
+        public_id (int): Public ID of the deletable type
+
+    Raises:
+        ManagerGetError: When the type with the `public_id` was not found.
+        ManagerDeleteError: When something went wrong during the deletion.
+
+    Returns:
+        DeleteSingleResponse: Delete result with the deleted type as data.
+    """
     type_manager = TypeManager(database_manager=current_app.database_manager)
     try:
         deleted_type = type_manager.delete(public_id=PublicID(public_id))
