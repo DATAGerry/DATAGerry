1643b6ead65f48422928dc49f5badc92355c6021
Dorus Blanken <dorusblanken@gmail.com>
2020-10-30 15:34:40 +0100

    [NET-594] Generates QR Code for link based on user input

--- app/src/app/layout/components/feedback-form/feedback-form.component.ts
+++ app/src/app/layout/components/feedback-form/feedback-form.component.ts
@@ -1,21 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { SystemService } from '../../../settings/system/system.service';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'cmdb-feedback-form',
   templateUrl: './feedback-form.component.html',
   styleUrls: ['./feedback-form.component.scss']
 })
-export class FeedbackFormComponent implements OnInit {
+export class FeedbackFormComponent implements OnInit, OnDestroy {
 
-  public systemInfo: any;
+  public feedbackForm: FormGroup;
+  public formListener: Subscription;
+  public feedbackUrl: string;
 
-  constructor(private systemService: SystemService) { }
-
-  ngOnInit() {
-    this.systemService.getDatagerryInformation().subscribe((infos: any[]) => {
-      this.systemInfo = infos;
+  constructor(private systemService: SystemService) {
+    this.feedbackForm = new FormGroup({
+      happiness: new FormControl(0),
+      usability: new FormControl(0),
+      functionality: new FormControl(0),
+      performance: new FormControl(0),
+      stability: new FormControl(0),
+      version: new FormControl(''),
+      email: new FormControl('', Validators.email),
     });
   }
 
+  ngOnInit() {
+    this.systemService.getDatagerryInformation().subscribe((infos: any) => {
+      this.feedbackForm.get('version').setValue(infos.version);
+    });
+
+    this.formListener = this.feedbackForm.valueChanges.subscribe(value => {
+      this.generateQRCode();
+    });
+  }
+
+  generateQRCode() {
+    console.log(this.feedbackForm);
+    let url = 'https://datagerry.com/feedback-v1/';
+    url = url + this.feedbackForm.get('happiness').value.toString() + '/';
+    url = url + this.feedbackForm.get('usability').value.toString() + '/';
+    url = url + this.feedbackForm.get('functionality').value.toString() + '/';
+    url = url + this.feedbackForm.get('performance').value.toString() + '/';
+    url = url + this.feedbackForm.get('stability').value.toString() + '/';
+    url = url + this.feedbackForm.get('version').value.toString() + '/';
+    url = url + this.feedbackForm.get('email').value.toString() + '/';
+    this.feedbackUrl = url;
+  }
+
+  ngOnDestroy() {
+    this.formListener.unsubscribe();
+  }
 }
