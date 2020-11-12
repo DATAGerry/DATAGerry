6e4c01073f0c019f9f43b8330b7d762a0b189a57
Mark Heumüller <mark.heumueller@nethinks.com>
2020-11-12 15:49:49 +0100

    [NET-701] Reset object service to nativ call

--- app/src/app/framework/services/object.service.ts
+++ app/src/app/framework/services/object.service.ts
@@ -102,7 +102,14 @@ export class ObjectService<T = CmdbObject | RenderResult> implements ApiService 
   public getObject<R>(publicID: number, native: boolean = false): Observable<R> {
     const options = this.options;
     options.params = new HttpParams();
-    return this.api.callGet<R>(`${ this.servicePrefix }/${ publicID }/`, options).pipe(
+    if (native === true) {
+      return this.api.callGet<CmdbObject[]>(`${ this.servicePrefix }/${ publicID }/native/`, options).pipe(
+        map((apiResponse) => {
+          return apiResponse.body;
+        })
+      );
+    }
+    return this.api.callGet<R[]>(`${ this.servicePrefix }/${ publicID }/`, options).pipe(
       map((apiResponse) => {
         return apiResponse.body;
       })
