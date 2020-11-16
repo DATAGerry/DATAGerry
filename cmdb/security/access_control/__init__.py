4c86506f4462c2b62b78412363cee73bb005542b
Mark Heumüller <mark.heumueller@nethinks.com>
2020-11-16 12:12:13 +0100

    [NET-681][NET-709] Add basic acl crud functions to the module

--- cmdb/security/access_control/__init__.py
+++ cmdb/security/access_control/__init__.py
@@ -21,6 +21,7 @@ T = TypeVar('T')
 
 @unique
 class AccessControlPermission(Enum):
+    """Permission enum for possible ACL operations."""
     CREATE = auto()
     READ = auto()
     UPDATE = auto()
@@ -31,12 +32,22 @@ class AccessControlPermission(Enum):
 
 
 class AccessControlListEntry(Generic[T]):
-    def __init__(self, role: T, permissions: List[AccessControlPermission]):
+    """ACL entry inside the `AccessControlListSection.`"""
+
+    def __init__(self, role: T, permissions: List[AccessControlPermission] = None):
         self.role = role
         self.permissions = permissions or []
 
+    @classmethod
+    def from_entry(cls, role: T, permissions: AccessControlPermission = None):
+        """Creates a `AccessControlListEntry` from a role and a single permission."""
+        if permissions:
+            permissions = [permissions]
+        return cls(role=role, permissions=permissions)
+
 
 class AccessControlListSection(Generic[T]):
+    """`AccessControlListSection` are a config element inside the complete ac-list."""
 
     def __init__(self, activated: bool = False, include: List[AccessControlListEntry[T]] = None):
         self.activated = activated
@@ -52,15 +63,76 @@ class AccessControlListSection(Generic[T]):
             raise TypeError('`AccessControlListSection` only takes lists as include structure')
         self._include = value
 
+    def _add_entry(self, role: T, permission: AccessControlPermission = None) -> AccessControlListEntry[T]:
+        entry = AccessControlListEntry.from_entry(role, permission)
+        self.include.append(entry)
+        return entry
+
+    def _get_entry(self, role: T) -> AccessControlListEntry[T]:
+        for entry in self.include:
+            if entry.role == role:
+                return entry
+        raise ValueError('No entry in the list')
+
+    def _update_entry(self, entry: AccessControlListEntry[T]) -> List[AccessControlListEntry[T]]:
+        for idx, e in enumerate(self.include):
+            if e.role == entry.role:
+                self.include[idx] = entry
+                return self.include
+        else:
+            raise IndexError('Entry not exists')
+
+    def grant_access(self, role: T, permission: AccessControlPermission):
+        try:
+            entry = self._get_entry(role)
+        except ValueError:
+            entry = self._add_entry(role)
+        entry.permissions.append(permission)
+        self._update_entry(entry)
+
+    def revoke_access(self, role: T, permission: AccessControlPermission):
+        entry = self._get_entry(role)
+        entry.permissions.remove(permission)
+        self._update_entry(entry)
+
+    def verify_access(self, role: T, permission: AccessControlPermission) -> bool:
+        entry = self._get_entry(role)
+        return permission in entry.permissions
+
 
 class GroupACL(AccessControlListSection[int]):
+    """Wrapper class for the group section"""
 
     def __init__(self, activated: bool = False, include: List = None):
         super(GroupACL, self).__init__(activated=activated, include=include)
 
 
 class AccessControlList:
+    """
+    The actual implementation of the Access Control List (ACL).
+    """
 
     def __init__(self, activated: bool, groups: GroupACL = None):
         self.activated = activated
         self.groups: GroupACL = groups
+
+    def grant_access(self, section: str, role: T, permission: AccessControlPermission):
+        if section == 'groups':
+            self.groups.grant_access(role, permission)
+        else:
+            raise ValueError(f'No ACL section with name: {section}')
+
+    def revoke_access(self, section: str, role: T, permission: AccessControlPermission):
+        if section == 'groups':
+            self.groups.grant_access(role, permission)
+        else:
+            raise ValueError(f'No ACL section with name: {section}')
+
+    def verify_access(self, section: str, role: T, permission: AccessControlPermission) -> bool:
+        if section == 'groups':
+            return self.groups.verify_access(role, permission)
+        else:
+            raise ValueError(f'No ACL section with name: {section}')
+
+    def deny_access(self, role: T, permission: AccessControlPermission):
+        raise NotImplementedError
