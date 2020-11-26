a73119c2d2bc1071e204dcb3662a71467484bea9
Dorus Blanken <dorusblanken@gmail.com>
2020-11-26 10:40:49 +0100

    [NET-689] Minor Cosmetic changes and appended File Headers

--- app/src/app/framework/object/components/object-table-head/object-table-head.component.ts
+++ app/src/app/framework/object/components/object-table-head/object-table-head.component.ts
@@ -1,7 +1,23 @@
-import {Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output} from '@angular/core';
-import {CmdbType} from '../../../models/cmdb-type';
-import {Router} from '@angular/router';
+/*
+* DATAGERRY - OpenSource Enterprise CMDB
+* Copyright (C) 2020 NETHINKS GmbH
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
 
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CmdbType } from '../../../models/cmdb-type';
 @Component({
   selector: 'cmdb-object-table-head',
   templateUrl: './object-table-head.component.html',
