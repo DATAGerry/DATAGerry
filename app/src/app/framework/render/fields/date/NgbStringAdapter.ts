2f97ad8742457428c5c7ed7fcef9200ca98e6075
Sergej Dumler <sergej.dumler@nethinks.com>
2021-02-11 08:25:48 +0100

    [NET-351][NET-906] Format for date fields adapted.

--- app/src/app/framework/render/fields/date/NgbStringAdapter.ts
+++ app/src/app/framework/render/fields/date/NgbStringAdapter.ts
@@ -1,4 +1,22 @@
-import { NgbDateAdapter, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+/*
+* DATAGERRY - OpenSource Enterprise CMDB
+* Copyright (C) 2019 NETHINKS GmbH
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import {NgbDateAdapter, NgbDateParserFormatter, NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
 import { Injectable } from '@angular/core';
 
 @Injectable()
@@ -19,9 +37,9 @@ export class NgbStringAdapter extends NgbDateAdapter<Date> {
     } else if (date != null) {
       date = new Date(date.$date);
       return date ? {
-        year: date.getFullYear(),
+        day: date.getDate(),
         month: date.getMonth() + 1,
-        day: date.getDate()
+        year: date.getFullYear()
       } : null;
     }
     return null;
@@ -34,5 +52,47 @@ export class NgbStringAdapter extends NgbDateAdapter<Date> {
       return {$date: d.getTime()};
     }
     return null;
+  }
+}
+
+
+/**
+ * This Service handles how the date is rendered and parsed from keyboard i.e. in the bound input field.
+ */
+@Injectable()
+export class CustomDateParserFormatter extends NgbDateParserFormatter {
+
+  readonly DELIMITER = '/';
+
+  parse(value: string): NgbDateStruct | null {
+    if (value) {
+      const date = value.split(this.DELIMITER);
+      return {
+        day : parseInt(date[0], 10),
+        month : parseInt(date[1], 10),
+        year : parseInt(date[2], 10)
+      };
+    }
+    return null;
+  }
+
+  padNumber(value: number) {
+    if (this.isNumber(value)) {
+      return `0${value}`.slice(-2);
+    } else {
+      return value;
+    }
+  }
+
+  isNumber(value: any): boolean {
+    return !isNaN(this.toInteger(value));
+  }
+
+  toInteger(value: any): number {
+    return parseInt(`${value}`, 10);
+  }
+
+  format(date: NgbDateStruct | null): string {
+    return date ? date.day + this.DELIMITER + this.padNumber(date.month) + this.DELIMITER + date.year : null;
   }
 }
--- app/src/app/framework/render/fields/date/date.component.ts
+++ app/src/app/framework/render/fields/date/date.component.ts
@@ -1,14 +1,36 @@
+/*
+* DATAGERRY - OpenSource Enterprise CMDB
+* Copyright (C) 2019 NETHINKS GmbH
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
 import { Component, OnInit } from '@angular/core';
 import { RenderFieldComponent } from '../components.fields';
 import { formatDate } from '@angular/common';
-import { NgbDateAdapter, NgbDateNativeAdapter, NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
-import { NgbStringAdapter } from './NgbStringAdapter';
+import { NgbDateAdapter, NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
+import { CustomDateParserFormatter, NgbStringAdapter } from './NgbStringAdapter';
 
 @Component({
   selector: 'cmdb-date',
   templateUrl: './date.component.html',
   styleUrls: ['./date.component.scss'],
-  providers: [{provide: NgbDateAdapter, useClass: NgbStringAdapter}]
+  providers: [
+    {provide: NgbDateAdapter, useClass: NgbStringAdapter},
+    {provide: NgbDateParserFormatter, useClass: CustomDateParserFormatter}
+    ]
 })
 export class DateComponent extends RenderFieldComponent implements  OnInit {
 
