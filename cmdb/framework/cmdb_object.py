1fa2e12c3882494c9b3e36dc240d7926acc10e71
Mark Heumüller <mark.heumueller@nethinks.com>
2021-02-15 21:50:33 +0100

    [NET-886] Add fuzzy parsing to creation time and last edit time

--- cmdb/framework/cmdb_object.py
+++ cmdb/framework/cmdb_object.py
@@ -1,5 +1,5 @@
 # DATAGERRY - OpenSource Enterprise CMDB
-# Copyright (C) 2019 NETHINKS GmbH
+# Copyright (C) 2019 - 2021 NETHINKS GmbH
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -12,13 +12,14 @@
 # GNU Affero General Public License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 import logging
 
 from cmdb.framework.cmdb_dao import CmdbDAO
 from cmdb.framework.cmdb_errors import FieldNotFoundError
 from cmdb.framework.utils import Collection, Model
+from dateutil.parser import parse
 
 try:
     from cmdb.utils.error import CMDBError
@@ -77,12 +78,18 @@ class CmdbObject(CmdbDAO):
 
     @classmethod
     def from_data(cls, data: dict, *args, **kwargs) -> "CmdbObject":
+        creation_time = data.get('creation_time', None)
+        last_edit_time = data.get('last_edit_time', None)
+        if creation_time and isinstance(creation_time, str):
+            creation_time = parse(creation_time, fuzzy=True)
+        if last_edit_time and isinstance(last_edit_time, str):
+            last_edit_time = parse(last_edit_time, fuzzy=True)
         return cls(
             type_id=data.get('type_id'),
             version=data.get('version'),
-            creation_time=data.get('creation_time'),
+            creation_time=creation_time,
             author_id=data.get('author_id'),
-            last_edit_time=data.get('last_edit_time', None),
+            last_edit_time=last_edit_time,
             editor_id=data.get('editor_id', None),
             active=data.get('active'),
             fields=data.get('fields', []),
