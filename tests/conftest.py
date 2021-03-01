54394644f83cacdf5aac17c5389f209316f8c3ea
Mark Heumüller <mark.heumueller@nethinks.com>
2021-03-01 13:30:58 +0100

    [NET-947][NET-948] Set database default for pytest to cmdb-test

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,5 +9,5 @@ def pytest_addoption(parser):
         '--mongodb-port', action='store', default=27017, help='Port of mongodb test instance'
     )
     parser.addoption(
-        '--mongodb-database', action='store', default='data-quality', help='Database of mongodb test instance'
+        '--mongodb-database', action='store', default='cmdb-test', help='Database of mongodb test instance'
     )
