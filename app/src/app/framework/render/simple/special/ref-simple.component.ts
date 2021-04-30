e1ae2e20e94c9fb4445977a83b94dc1afa512c96
Sergej Dumler <sergej.dumler@nethinks.com>
2021-04-30 09:09:12 +0200

    [NET-1026] Cleanup: Warnings browser console.

--- app/src/app/framework/render/simple/special/ref-simple.component.ts
+++ app/src/app/framework/render/simple/special/ref-simple.component.ts
@@ -1,6 +1,6 @@
 /*
 * DATAGERRY - OpenSource Enterprise CMDB
-* Copyright (C) 2019 NETHINKS GmbH
+* Copyright (C) 2019 - 2021 NETHINKS GmbH
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU Affero General Public License as
@@ -51,8 +51,9 @@ export class RefSimpleComponent extends RenderFieldComponent implements OnInit {
           this.refData = {
             reference: new TypeReference({
               type_id: res.type_information.type_id,
+              object_id: res.object_information.object_id,
               icon: res.type_information.icon,
-              label: res.type_information.type_label,
+              type_label: res.type_information.type_label,
               summaries: res.summaries
             }),
             value: this.data.value,
