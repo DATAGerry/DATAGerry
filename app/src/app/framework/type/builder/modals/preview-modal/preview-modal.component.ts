bfccb31c73d6d6910c2e9167e287437b9302354c
Sergej Dumler <sergej.dumler@nethinks.com>
2021-08-16 15:39:44 +0200

    [NET-1112] Preview / Diagnostic view restored in the type generator.

---
+++ app/src/app/framework/type/builder/modals/preview-modal/preview-modal.component.ts
@@ -0,0 +1,39 @@
+/*
+* DATAGERRY - OpenSource Enterprise CMDB
+* Copyright (C) 2019 NETHINKS GmbH
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import { Component, Input } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { CmdbMode } from '../../../../modes.enum';
+import { FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'cmdb-preview-modal',
+  templateUrl: './preview-modal.component.html',
+  styleUrls: ['./preview-modal.component.scss']
+})
+export class PreviewModalComponent {
+
+  @Input() sections: any[];
+  public renderForm: FormGroup;
+  public modes = CmdbMode;
+
+  constructor(public activeModal: NgbActiveModal) {
+    this.renderForm = new FormGroup({});
+  }
+
+}
