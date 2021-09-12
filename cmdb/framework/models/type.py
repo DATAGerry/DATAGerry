505d34ac01576f785371823dff5de3e7110309ab
Sergej Dumler <sergej.dumler@nethinks.com>
2021-09-12 08:19:47 +0200

    [NET-1120] Allow list for type references

--- cmdb/framework/models/type.py
+++ cmdb/framework/models/type.py
@@ -472,7 +472,7 @@ class TypeModel(CmdbDAO):
                         'required': False,
                         'empty': True,
                         'schema': {
-                            'type': 'integer',
+                            'type': ['integer', 'list'],
                         }
                     },
                     "summaries": {
