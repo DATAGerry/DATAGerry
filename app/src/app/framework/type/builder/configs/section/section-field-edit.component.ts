d80680a000cf5899435c9d2dd92d9f5b876777d2
Abdullah Khalid <Abdullah.Khalid@becon.de>
2023-12-14 19:33:45 +0100

    solved save button never reaches a valid state

--- app/src/app/framework/type/builder/configs/section/section-field-edit.component.ts
+++ app/src/app/framework/type/builder/configs/section/section-field-edit.component.ts
@@ -55,8 +55,6 @@ export class SectionFieldEditComponent extends ConfigEditBaseComponent implement
     this.form.addControl('label', this.labelControl);
 
     this.disableControlOnEdit(this.nameControl);
-    this.disableControlsOnGlobal(this.nameControl);
-    this.disableControlsOnGlobal(this.labelControl);
     this.patchData(this.data, this.form);
     this.initialValue = this.nameControl.value;
   }
