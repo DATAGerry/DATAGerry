f491afd6e86e4b0622187b96a8cc5692725d3731
Abdullah Khalid <Abdullah.Khalid@becon.de>
2024-01-21 23:27:52 +0100

    Copyright Headers Backend Updated to 2024

--- app/src/app/layout/table/components/table-search/table-search.component.ts
+++ app/src/app/layout/table/components/table-search/table-search.component.ts
@@ -1,6 +1,6 @@
 /*
 * DATAGERRY - OpenSource Enterprise CMDB
-* Copyright (C) 2023 becon GmbH
+* Copyright (C) 2024 becon GmbH
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU Affero General Public License as
