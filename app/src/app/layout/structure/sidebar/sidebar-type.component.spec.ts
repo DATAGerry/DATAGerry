64acf17f0c36f190b08363dd64ab3bb64e0fb093
Abdullah Khalid <Abdullah.Khalid@becon.de>
2024-07-14 23:34:58 +0200

    removed imports

--- app/src/app/layout/structure/sidebar/sidebar-type.component.spec.ts
+++ app/src/app/layout/structure/sidebar/sidebar-type.component.spec.ts
@@ -2,9 +2,6 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SidebarTypeComponent } from './sidebar-type.component';
 
 import { By } from '@angular/platform-browser';
-import { RouterTestingModule } from '@angular/router/testing';
-import { CmdbType } from 'src/app/framework/models/cmdb-type';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ObjectService } from 'src/app/framework/services/object.service';
 import { SidebarService } from '../../services/sidebar.service';
 
@@ -28,24 +25,29 @@ describe('SidebarTypeComponent', () => {
         }).compileComponents();
     });
 
+
     beforeEach(() => {
         fixture = TestBed.createComponent(SidebarTypeComponent);
         component = fixture.componentInstance;
     });
 
+
     it('should create the component', () => {
         expect(component).toBeTruthy();
     });
+
 
     it('should initialize counter on init', () => {
         component.ngOnInit();
         expect(mockSidebarService.initializeCounter).toHaveBeenCalledWith(component);
     });
 
+
     it('should delete counter on destroy', () => {
         component.ngOnDestroy();
         expect(mockSidebarService.deleteCounter).toHaveBeenCalledWith(component);
     });
+
 
     it('should not render if type is not provided', () => {
         component.type = null;
